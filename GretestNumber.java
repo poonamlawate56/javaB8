14c3ecc5555a34334ccfd5ce74a77396d4cbb645
poonamlawate56 <125649394+poonamlawate56@users.noreply.github.com>
2023-07-27 16:18:57 +0530

    Update GretestNumber.java

--- GretestNumber.java
+++ GretestNumber.java
@@ -17,7 +17,7 @@ public class GretestNumber {
 	public static void main(String[] args) {
 	
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Hello");
+		
 		System.out.println("Enter three numbers->");
 		int num1=sc.nextInt();
 		int num2=sc.nextInt();
